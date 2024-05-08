f13319e69b4e9be397a1759649f8e567429c7425
taxevader007 <santiagoespinosagiraldo1@gmail.com>
2024-05-08 17:50:13 -0500

    think is finished

--- src/main/java/collections/Graph.java
+++ src/main/java/collections/Graph.java
@@ -2,19 +2,36 @@ package collections;
 
 import java.util.*;
 
+/**
+ * This class represents a generic Graph data structure.
+ * @param <T> the type of elements in this Graph.
+ */
 public class Graph <T> {
 
     private HashMap<T, LinkedList<T>> map;
     private List<T> dfs_vis = new ArrayList<>();
 
+    /**
+     * Constructs an empty Graph.
+     */
     public Graph() {
         map = new HashMap<>();
     }
 
+    /**
+     * Adds a vertex to the Graph.
+     * @param label the vertex to be added.
+     */
     public void addVertex(T label) {
         map.put(label, new LinkedList<T>());
     }
 
+    /**
+     * Adds an edge to the Graph.
+     * @param init the initial vertex of the edge.
+     * @param fin the final vertex of the edge.
+     * @param undirected true if the edge is undirected, false otherwise.
+     */
     public void addEdge(T init, T fin, boolean undirected) {
         if(!map.containsKey(init)) {
             addVertex(init);
@@ -31,40 +48,57 @@ public class Graph <T> {
         }
     }
 
+    /**
+     * Checks if a vertex is in the Graph.
+     * @param v the vertex to check.
+     * @return true if the vertex is in the Graph, false otherwise.
+     */
     public boolean hasVertex(T v) {
         return map.containsKey(v);
     }
 
+    /**
+     * Checks if an edge is in the Graph.
+     * @param src the source vertex of the edge.
+     * @param des the destination vertex of the edge.
+     * @return true if the edge is in the Graph, false otherwise.
+     */
     public boolean hasEdge(T src, T des) {
         return map.get(src).contains(des);
     }
 
+    /**
+     * Returns the number of vertices in the Graph.
+     * @return the number of vertices in the Graph.
+     */
     public int numVertices() {
         return map.keySet().size();
     }
 
-
+    /**
+     * Returns a string representation of the Graph.
+     * @return a string representation of the Graph.
+     */
     @Override
     public String toString() {
-
         String s = "";
-
         for(T i: map.keySet()) {
             s += i.toString() + ":";
-
             for(T j: map.get(i)) {
                 s += j.toString() + " ";
             }
             s += "\n";
         }
-
         return s;
     }
 
+    /**
+     * Performs a depth-first search (DFS) starting from a given vertex.
+     * @param n the starting vertex.
+     * @return a list of vertices visited during the DFS.
+     */
     public List<T> dfs(T n) {
-
         dfs_vis.add(n);
-
         for(T i : map.get(n)) {
             if(!dfs_vis.contains(i)) {
                 dfs(i);
@@ -73,6 +107,11 @@ public class Graph <T> {
         return dfs_vis;
     }
 
+    /**
+     * Performs a breadth-first search (BFS) starting from a given vertex.
+     * @param n the starting vertex.
+     * @return a map where the keys are the vertices and the values are the distances from the starting vertex.
+     */
     public HashMap<T, Integer> bfs(T n) {
         int v = numVertices();
         Queue<T> q = new LinkedList<>();
@@ -85,7 +124,6 @@ public class Graph <T> {
 
         while(!q.isEmpty()) {
             T cur = q.poll();
-
             for(T i : map.get(cur)) {
                 if(!vis.contains(i)) {
                     vis.add(i);
@@ -94,21 +132,58 @@ public class Graph <T> {
                 }
             }
         }
-
         return dis;
     }
 
+
+
+    /**
+     * Checks if there is a path between two vertices.
+     * @param src the source vertex.
+     * @param des the destination vertex.
+     * @return true if there is a path, false otherwise.
+     */
     public boolean hasPath(T src, T des) {
         ArrayList<T> dfs_list = (ArrayList<T>) dfs(src);
-
-        if(dfs_list.contains(des)) {
-            return true;
-        } else {
-            return false;
-        }
-
+        return dfs_list.contains(des);
     }
 
+    /**
+     * Returns the adjacency matrix of the Graph.
+     * @return the adjacency matrix of the Graph.
+     */
+    public int[][] getAdjacencyMatrix() {
+        int size = map.size();
+        int[][] matrix = new int[size][size];
+        List<T> nodes = new ArrayList<>(map.keySet());
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (map.get(nodes.get(i)).contains(nodes.get(j))) {
+                    matrix[i][j] = 1;
+                } else {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        return matrix;
+    }
+
+
+    /**
+     * Returns the adjacency list of the Graph.
+     * @return the adjacency list of the Graph.
+     */
+    public HashMap<T, List<T>> getAdjacencyList() {
+        HashMap<T, List<T>> copy = new HashMap<>();
+        for (Map.Entry<T, LinkedList<T>> entry : map.entrySet()) {
+            copy.put(entry.getKey(), new LinkedList<>(entry.getValue()));
+        }
+        return copy;
+    }
+
+
+    //implementar bfs y dfs , listas de adyacencia y matrices de adyacencia
 
 
 }
--- src/main/java/ui/main.java
+++ src/main/java/ui/main.java
@@ -1,8 +1,36 @@
 package ui;
 
+
+import collections.*;
+import java.util.*;
+
 public class main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Graph<String> graph = new Graph<>();
+        graph.addVertex("A");
+        graph.addVertex("B");
+        graph.addVertex("C");
+        graph.addEdge("A", "B", true);
+        graph.addEdge("B", "C", true);
+
+
+        //dfs
+        List<String> dfsResult = graph.dfs("A");
+        for (String node : dfsResult) {
+            System.out.println("Node: " + node);
+        }
+
+        //bfs
+
+
+        //adjacent matrix
+        int[][] adjacencyMatrix = graph.getAdjacencyMatrix();
+        for (int i = 0; i < adjacencyMatrix.length; i++) {
+            for (int j = 0; j < adjacencyMatrix[i].length; j++) {
+                System.out.print(adjacencyMatrix[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
